edffcf49c3a36a0a843ae3693caf5a8752933bfe
Parkash Kumar <parkashkumar.bscs.official@gmail.com>
2025-03-03 16:55:33 +0500

    Responsive_Basic Practice updated2  03/03/2025

--- Basic_Responsive_Practices/logic.js
+++ Basic_Responsive_Practices/logic.js
@@ -1,3 +1,16 @@
-function resizeScreen(val){
-    
+function resizeScreen(){
+    let scWidth = window.innerWidth;
+    document.getElementById('span').innerText = scWidth;
+    if(scWidth >= 320 && scWidth <= 480){
+        document.getElementById('dis').innerText = "Mobile Size";
+    }
+    else if(scWidth >480  && scWidth <= 1024){
+        document.getElementById('dis').innerText = "Tablet Size";
+    }
+    else if(scWidth > 1024){
+        document.getElementById('dis').innerText = "Desktop Size";
+    }
+    else{
+        document.getElementById('dis').innerText = "";
+    }
 }
